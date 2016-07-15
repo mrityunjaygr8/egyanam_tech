f3c06e4821c98cd0a2b21eee80de5bad834009c2
mritynjaygr8 <mrityunjay.saxena.met14@itbhu.ac.in>
2016-07-15 20:10:01 +0530

    viewport meta added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-require('newrelic');
 var dotenv = require('dotenv');
 dotenv.load();
+require('newrelic');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
