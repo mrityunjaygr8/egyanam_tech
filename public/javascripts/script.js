e3cc083ea7aa4ded5f10617260eb92115fd37b63
mritynjaygr8 <mrityunjay.saxena.met14@itbhu.ac.in>
2016-07-21 10:20:53 +0530

    side menu adapted

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,27 @@
 $(function() {
 
+    var tri = function(obj){
+        if(obj.children.length >= 2 && obj.children[1].tagName === 'CANVAS'){
+            obj.removeChild(obj.children[1]);
+        }
+        var pattern = Trianglify({
+            height: obj.clientHeight,
+            width: obj.clientWidth,
+            cell_size: 45,
+            seed: '#f20a41'
+        });
+
+        obj.appendChild(pattern.canvas());
+    };
+
+    window.onload = function(){
+        tri(document.querySelector('.left'));
+    };
+
+    window.onresize = function(){
+        tri(document.querySelector('.left'));
+    };
+
     $('#scotch-panel').scotchPanel({
         containerSelector: '#site-wrapper',
         direction: 'right',
@@ -10,21 +32,23 @@ $(function() {
         enableEscapeKey: true,
 
         beforePanelOpen: function() {
-            $('header').css({
+            $('header, .out').css({
                 '-moz-transform': 'translate3d(-30%, 0, 0)',
                 '-ms-transform': 'translate3d(-30%, 0, 0)',
                 '-o-transform': 'translate3d(-30%, 0, 0)',
                 '-webkit-transform': 'translate3d(-30%, 0, 0)',
-                'transform': 'translate3d(-30%, 0, 0)'
+                'transform': 'translate3d(-30%, 0, 0)',
+                'transition': 'all 300ms ease'
             });
         },
         beforePanelClose: function() {
-            $('header').css({
+            $('header, .out').css({
                 '-moz-transform': 'translate3d(0, 0, 0)',
                 '-ms-transform': 'translate3d(0, 0, 0)',
                 '-o-transform': 'translate3d(0, 0, 0)',
                 '-webkit-transform': 'translate3d(0, 0, 0)',
-                'transform': 'translate3d(0, 0, 0)'
+                'transform': 'translate3d(0, 0, 0)',
+                'transition': 'all 300ms ease'
             });
         }
     });
