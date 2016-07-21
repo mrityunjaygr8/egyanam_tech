79899c6d3d3c5c0847f55b565c1bb95486460e88
Mrityunjay Saxena <mrityunjay.saxena.met14@iitbhu.ac.in>
2016-07-21 23:54:59 +0530

    saving stuff on mac to go to mine

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -29,20 +29,20 @@ $(function() {
     $('#scotch-panel').scotchPanel({
         containerSelector: '#site-wrapper',
         direction: 'right',
-        duration: 300,
+        duration: 400,
         transition: 'ease',
         clickSelector: '.toggle-panel',
-        distanceX: '30%',
+        distanceX: '40%',
         enableEscapeKey: true,
 
         beforePanelOpen: function() {
             $('header, .out').css({
-                '-moz-transform': 'translate3d(-30%, 0, 0)',
-                '-ms-transform': 'translate3d(-30%, 0, 0)',
-                '-o-transform': 'translate3d(-30%, 0, 0)',
-                '-webkit-transform': 'translate3d(-30%, 0, 0)',
-                'transform': 'translate3d(-30%, 0, 0)',
-                'transition': 'all 300ms ease'
+                '-moz-transform': 'translate3d(-40%, 0, 0)',
+                '-ms-transform': 'translate3d(-40%, 0, 0)',
+                '-o-transform': 'translate3d(-40%, 0, 0)',
+                '-webkit-transform': 'translate3d(-40%, 0, 0)',
+                'transform': 'translate3d(-40%, 0, 0)',
+                'transition': 'all 400ms ease'
             });
         },
         beforePanelClose: function() {
@@ -52,7 +52,7 @@ $(function() {
                 '-o-transform': 'translate3d(0, 0, 0)',
                 '-webkit-transform': 'translate3d(0, 0, 0)',
                 'transform': 'translate3d(0, 0, 0)',
-                'transition': 'all 300ms ease'
+                'transition': 'all 400ms ease'
             });
         }
     });
